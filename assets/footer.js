5cfbd407c24d3be26a4902e3e888e75896c802d0
Sonja Ericsson <sonjae@spotify.com>
2025-08-05 13:41:33 +0200

    fix

--- assets/footer.js
+++ assets/footer.js
@@ -5,7 +5,7 @@ class Footer {
       <footer>
         <div class="container">
           <div class="footer-content">
-            <p>Grundbulten 144568 AB| Organisational Number: 559536-6823 | D-U-N-S® Number: 351516756 </p>
+            <p>Grundbulten 144568 AB | Organisational Number: 559536-6823 | D-U-N-S® Number: 351516756 </p>
             <p>&copy; 2025 Color Aura. All rights reserved.</p>
           </div>
         </div>
